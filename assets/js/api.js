4840933f4fb0bf1055c382f6b09ea1a9e78f4fff
Kelcey Asuao <kelceyasuao@gmail.com>
2017-08-02 21:38:21 -0600

    second commit

---
+++ assets/js/api.js
@@ -0,0 +1,22 @@
+ //===================================================
+ //connect to firebase
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB8ssYN0NY7kolATwvynIfBquGiSFy-Q0M",
+    authDomain: "esen-dfd9c.firebaseapp.com",
+    databaseURL: "https://esen-dfd9c.firebaseio.com",
+    projectId: "esen-dfd9c",
+    storageBucket: "",
+    messagingSenderId: "51140866720"
+  };
+  firebase.initializeApp(config);
+
+ //sign-up
+  //Variables
+  var username;
+  var email;
+  var password;
+  var inputEmail;
+  var inputPw;
+  var database = database.firebase;
+//====================================================
