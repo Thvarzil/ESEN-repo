261c05d74693178219c531db685ae7365c9e98c2
Kelcey Asuao <kelceyasuao@gmail.com>
2017-08-05 11:53:23 -0600

    fourth commit

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -12,12 +12,10 @@
 
  //sign-up
   //Variables
-  var email;
-  var password;
-  var inputEmail;
-  var inputPw;
-   var database = firebase.database();
+  var database = firebase.database();
   var userlist = null;
+
+//==================NOT SURE WHAT THIS DOES======================
 //below says that it is referencing the /user branch in firebase
 //snapshot...I don't know what that means.
 database.ref("/users").on("value", function(snapshot) {
@@ -26,46 +24,74 @@ database.ref("/users").on("value", function(snapshot) {
 }, function(errorObject) {
   console.log("The read failed: " + errorObject.code);
 });
+//=================================================================
 
+//===============ON CLICK on SIGN UP FORM====================================
   //collect data from form and store in firebase
   $("#add-user").on("click", function(noReset){
     noReset.preventDefault();
     //store variables
     var inputuser = $("#name-input").val().trim();
       console.log(inputuser);
-    var email = $("#email-input").val().trim();
-      console.log(email);
-    var password = $("#password-input").val().trim();
-      console.log(password);
-    //add to array
-   //setting userExists at false so that second if function doesn' run by default.
+    var inputemail = $("#email-input").val().trim();
+      console.log(inputemail);
+    var inputpassword = $("#password-input").val().trim();
+      console.log(inputpassword);
+    //===================adding LOGIN=================================
+    //setting userExists at false so that second if function doesn' run by default.
     var userExists = false;
+    var emailused = false;
     //using a for each loop because firebase doesn't like arrays
     //the parameters key and value are default. They refer to what you would see in a table.
     $.each( userlist, function( key, value ) {
-      if(value.username === inputuser){
-        alert("user exsits");
+      if(value.username === inputuser && value.email === inputemail){
+        alert("Username or email already used. Please try again.");
         userExists= true;
-      }
-});
+        emailused = true;
+        }
+      });
     //The !userExists means that userExists is set to false
-    if(!userExists){
+    if(!userExists && !emailused){
       //pushes info to the database
-      alert("user added!");
+      alert("User added!");
         database.ref("/users").push({
       username : inputuser,
-      eMail: email,
-      passWord: password,
+      email: inputemail,
+      passWord: inputpassword,
     }); 
-    // 
-      }
+    }
+    //==================end of 'adding LOGIN'===================
+});
+//======================END OF SIGN UP ON CLICK=========================================
+
+
+
+//=====================MEMBER SIGN IN=====================
+//collect data from form and store in firebase
+  $("#sigin").on("click", function(noReset){
+    noReset.preventDefault();
+    //store variables
+    var memberemail = $("#email-signin").val().trim();
+      console.log(memberemail);
+    var memberpassword = $("#password-signin").val().trim();
+      console.log(memberpassword);
+
+    //===================adding LOGIN=================================
+    //setting userExists at false so that second if function doesn' run by default.
+    var emailvalid = false;
+    var passwordvalid = false;
+    //using a for each loop because firebase doesn't like arrays
+    //the parameters key and value are default. They refer to what you would see in a table.
+    $.each( userlist, function( key, value ) {
+      if(value.email === memberemail && value.passWord === memberpassword){
+        alert("Welcome to the ESEN. Your page will be loaded shortly.");
+        emailvalid = true;
+        passwordvalid = true;
+        }
+      });
+    //The !emailvalid means that emailvalid is set to false
+    if(!emailvalid && !passwordvalid){
+      alert("Your Email or Password is not correct.");
+    };
   });
-//member signin
-  //temp store user input
-    //temp store user email
-    //temp store user password
-  //compare
-    //if input email and input password matches 
-    //a stored email and password
-    //load next page
-    //else give alert "email or password does not match...try again".
+//======================end of member sign in==============
